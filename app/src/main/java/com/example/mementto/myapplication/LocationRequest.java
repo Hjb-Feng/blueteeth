a545140bd4a09c039d81094dbb303976c2250ec5
mementto <63023704@qq.com>
2019-02-12 14:58:55 +0800

    打开蓝牙

---
+++ app/src/main/java/com/example/mementto/myapplication/LocationRequest.java
@@ -0,0 +1,17 @@
+package com.example.mementto.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+public class LocationRequest {
+
+    private static final int REQUEST_COARSE_LOCATION = 0;
+
+
+
+}
---
+++ app/src/main/java/com/example/mementto/myapplication/StringValue.java
@@ -0,0 +1,14 @@
+package com.example.mementto.myapplication;
+
+public class StringValue {
+
+    public static String WITHOUT_BT = "该设备可能没有蓝牙";
+
+    public static String VERSION_VALUE = "安卓版本过低";
+
+    public static String VERSION_REJECTED = "权限被拒绝";
+
+    public static String VERSION_AWARDED = "权限被授予";
+
+    public static String BT_SCAN_FAIL ="蓝牙扫描失败";
+}
